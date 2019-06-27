6ffd2e479aaa8c03cc98a87dd4a4ed0ad9e0e8d6
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-27 16:02:16 -0700

    Enhance products class component to get data from products file and update the state.#1

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,6 +1,15 @@
 import React from "react"
+import { getProducts } from "../services/fakeProductService"
 
 export default class Products extends React.Component {
+  state = {
+    products: []
+  }
+  componentDidMount() {
+    let products = [...this.state.products]
+    products = getProducts()
+    this.setState({ products })
+  }
   render() {
     return <h1>Products</h1>
   }
