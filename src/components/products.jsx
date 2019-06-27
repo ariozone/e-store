c48595e939283c436e157b03f0a2fad25784dbf0
Ario Tamaddoni <ariozone6@gmail.com>
2019-06-27 16:43:15 -0700

    Enhance the grid to render a card for each product.#1

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -14,9 +14,20 @@ export default class Products extends React.Component {
     return (
       <React.Fragment>
         <h1>Products</h1>
-        <div className="row">
+        <div className="row" height="50">
           {this.state.products.map(p => (
-            <div className="card">{p.name}</div>
+            <div className="col-lg-4 col-md-12>">
+              <div className="card mb-4">
+                <img className=" card-img-top" src={p.image} alt="Product" />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.price}</p>
+                  <p className="card-text">
+                    <small className="text-muted">{p.numberInStock}</small>
+                  </p>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </React.Fragment>
