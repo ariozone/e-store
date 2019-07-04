952c5090cadb35a8148881c816411223fca34f16
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-04 14:33:26 -0700

    Import route from react-router-dom and other modules to App.js.#4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react"
 import Products from "./components/products"
 import "./App.css"
+import { Route } from "react-router-dom"
+import Customers from "./components/customers"
+import ProductForm from "./components/productForm"
+import Sales from "./components/sales"
+import NotFound from "./components/notFound"
 
 function App() {
   return (
