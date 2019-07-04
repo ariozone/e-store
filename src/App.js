20fdb74279cc63ff2843ec0af25237851fd5a56a
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-04 14:44:22 -0700

    Wrap the route elements in a switch element.#4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Products from "./components/products"
 import "./App.css"
-import { Route } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import Customers from "./components/customers"
 import ProductForm from "./components/productForm"
 import Sales from "./components/sales"
@@ -10,11 +10,13 @@ import NotFound from "./components/notFound"
 function App() {
   return (
     <div className="container">
-      <Route path={"/products"} component={Products} />
-      <Route path={"/customers"} component={Customers} />
-      <Route path={"/productForm"} component={ProductForm} />
-      <Route path={"/sales"} component={Sales} />
-      <Route path={"/notFound"} component={NotFound} />
+      <Switch>
+        <Route path={"/products"} component={Products} />
+        <Route path={"/customers"} component={Customers} />
+        <Route path={"/productForm"} component={ProductForm} />
+        <Route path={"/sales"} component={Sales} />
+        <Route path={"/notFound"} component={NotFound} />
+      </Switch>
     </div>
   )
 }
