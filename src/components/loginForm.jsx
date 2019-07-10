e64da73e37c9f3cd2b4681ca7d28ad1e0c4f50f4
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-09 17:57:03 -0700

    Add id attribute to input and modify htmlFor attribute of label.#5

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -6,21 +6,21 @@ export default class LoginForm extends Component {
         <h1>Login Form</h1>
         <form className="my-5">
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="username">Email address</label>
             <input
               type="email"
               className="form-control"
-              id="exampleInputEmail1"
+              id="username"
               aria-describedby="emailHelp"
               placeholder="Enter email"
             />
           </div>
           <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               className="form-control"
-              id="exampleInputPassword1"
+              id="password"
               placeholder="Password"
             />
           </div>
