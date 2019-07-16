1d1d605fdd412e5b0c008b871b655474e88c26a4
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-15 21:05:54 -0700

    Import Joi and create an schema to validate inputs.#6

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,7 +1,24 @@
 import React from "react"
 import Form from "../components/common/form"
+import Joi from "joi-browser"
+
 export default class RegisterForm extends Form {
   state = { data: { username: "", password: "", name: "" }, errors: {} }
+  schema = {
+    username: Joi.string()
+      .required()
+      .min(5)
+      .email()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .min(2)
+      .label("Name")
+  }
   render() {
     return (
       <div>
