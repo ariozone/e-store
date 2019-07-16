1f85e7fd11cbf67bb885dc2352c160c9f24bac8f
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-15 18:16:01 -0700

    Create a new module for reusable Form class component that LoginForm extends from it.#5

--- src/services/fakeProductService.js
+++ src/services/fakeProductService.js
@@ -18,7 +18,7 @@ const products = [
     numberInStock: 5,
     image:
       "https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222?b=1&k=6&m=938742222&s=170667a&w=0&h=px8dFDNHHwmCGbnyGnSGcTOerEG7-493JqZ39rlPWWw=",
-    price: 2.5
+    price: 9.5
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
@@ -27,7 +27,7 @@ const products = [
     numberInStock: 8,
     image:
       "https://images.pexels.com/photos/38325/vacuum-cleaner-carpet-cleaner-housework-housekeeping-38325.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 135
   },
   {
     _id: "5b21ca3eeb7f6fbccd471819",
@@ -36,7 +36,7 @@ const products = [
     numberInStock: 7,
     image:
       "https://images.pexels.com/photos/1432039/pexels-photo-1432039.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 115
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181a",
@@ -45,7 +45,7 @@ const products = [
     numberInStock: 7,
     image:
       "https://images.pexels.com/photos/424766/pexels-photo-424766.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 207
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
@@ -54,7 +54,7 @@ const products = [
     numberInStock: 7,
     image:
       "https://images.pexels.com/photos/274385/pexels-photo-274385.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 55
   },
   {
     _id: "5b21ca3eeb7f6fbccd47281f",
@@ -63,7 +63,7 @@ const products = [
     numberInStock: 7,
     image:
       "https://images.pexels.com/photos/274506/pexels-photo-274506.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 15
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
@@ -72,7 +72,7 @@ const products = [
     numberInStock: 7,
     image:
       "https://images.pexels.com/photos/60058/iron-home-appliances-small-appliances-60058.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 4.5
+    price: 24
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181f",
@@ -81,7 +81,7 @@ const products = [
     numberInStock: 4,
     image:
       "https://images.pexels.com/photos/1072851/pexels-photo-1072851.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 35
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821",
@@ -99,7 +99,7 @@ const products = [
     numberInStock: 3,
     image:
       "https://images.pexels.com/photos/72160/bbq-dinner-grilled-grill-72160.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 13.5
   },
   {
     _id: "5b21ca3eeb7f6fbccd371711",
@@ -108,7 +108,7 @@ const products = [
     numberInStock: 5,
     image:
       "https://images.pexels.com/photos/1234535/pexels-photo-1234535.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500",
-    price: 3.5
+    price: 5
   }
 ]
 
