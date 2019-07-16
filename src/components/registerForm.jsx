1794b927b027cfb7eaf45985a7e15a5c3d6b1c5d
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-15 20:42:48 -0700

    Add data and error properties to the state and add username, password and name properties to data.#6

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,3 +1,5 @@
 import React, { Component } from "react"
 import Form from "../components/common/form"
-export default class RegisterForm extends Form {}
+export default class RegisterForm extends Form {
+  state = { data: { username: "", password: "", name: "" }, errors: {} }
+}
