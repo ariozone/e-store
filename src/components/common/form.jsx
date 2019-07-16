46202ce590d42cb340ad1c44abc267b3f39991d2
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-15 18:18:34 -0700

    Move validate, validateOnChange, handleSubmit and handleChange methods to Form class component.#5

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,48 @@
 import React, { Component } from "react"
 
 export default class Form extends Component {
+  handleSubmit = e => {
+    e.preventDefault()
+    const errors = this.validate()
+    this.setState({ errors: errors || {} })
+    if (errors) return
+    console.log("Submited!")
+  }
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors }
+    const errorMessage = this.validateOnChange(input)
+    if (errorMessage) errors[input.name] = errorMessage
+    else delete errors[input.name]
+
+    const data = {
+      ...this.state.data
+    }
+    data[input.name] = input.value
+
+    this.setState({ data, errors })
+  }
+
+  validate = () => {
+    const { error } = Joi.validate(this.state.data, this.schema, {
+      abortEarly: false
+    })
+    if (!error) return null
+    const errors = {}
+    for (let property of error.details) {
+      errors[property.path[0]] = property.message
+    }
+
+    return errors
+  }
+
+  validateOnChange = input => {
+    const targetInput = { [input.name]: input.value } // computed properties in ES6
+    const schema = { [input.name]: this.schema[input.name] }
+    const { error } = Joi.validate(targetInput, schema)
+    return error ? error.details[0].message : null
+  }
+
   render() {
     return <h1>Form</h1>
   }
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -21,48 +21,6 @@ export default class LoginForm extends Component {
       .min(5)
   }
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const errors = this.validate()
-    this.setState({ errors: errors || {} })
-    if (errors) return
-    console.log("Submited!")
-  }
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors }
-    const errorMessage = this.validateOnChange(input)
-    if (errorMessage) errors[input.name] = errorMessage
-    else delete errors[input.name]
-
-    const data = {
-      ...this.state.data
-    }
-    data[input.name] = input.value
-
-    this.setState({ data, errors })
-  }
-
-  validate = () => {
-    const { error } = Joi.validate(this.state.data, this.schema, {
-      abortEarly: false
-    })
-    if (!error) return null
-    const errors = {}
-    for (let property of error.details) {
-      errors[property.path[0]] = property.message
-    }
-
-    return errors
-  }
-
-  validateOnChange = input => {
-    const targetInput = { [input.name]: input.value } // computed properties in ES6
-    const schema = { [input.name]: this.schema[input.name] }
-    const { error } = Joi.validate(targetInput, schema)
-    return error ? error.details[0].message : null
-  }
-
   render() {
     const { data, errors } = this.state
     return (
