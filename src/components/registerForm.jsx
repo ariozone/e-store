82ee795895a1e449c517c68565fa2cfa848d101a
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-15 20:59:34 -0700

    In register.jsx, enhance Register render method to return 3 inputs and a register button.#6

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,5 +1,18 @@
-import React, { Component } from "react"
+import React from "react"
 import Form from "../components/common/form"
 export default class RegisterForm extends Form {
   state = { data: { username: "", password: "", name: "" }, errors: {} }
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit} className="my-5">
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    )
+  }
 }
