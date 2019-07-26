7a3195e6a1f523c3ef11eabc4e6b62a9f5392cd1
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-25 19:27:39 -0700

    Enhance Select component to return bootstrap select component with given props.#13

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -1,7 +1,17 @@
 import React from "react"
 
-const Select = props => {
-  return <h1>Select</h1>
+const Select = ({ name, label, option, error, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <select className="form-control" id={name} name={name} {...rest}>
+        {option.map(o => (
+          <option key={o._id}>{o.name}</option>
+        ))}
+      </select>
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  )
 }
 
 export default Select
