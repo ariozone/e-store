d6e070fe303386f7e0f1591035269a8d27ed4f97
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-06 18:34:12 -0700

    Convert products class component to a functional component using react hooks.#13

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { getProducts } from "../services/fakeProductService"
 import ListGroup from "./common/listGroup"
 import { getCategories } from "../services/fakeCategoryService"
@@ -7,142 +7,127 @@ import { paginate } from "../utils/paginate"
 import Search from "./common/search"
 import { Link } from "react-router-dom"
 
-export default class Products extends React.Component {
-  state = {
-    products: [],
-    categories: [],
-    selectedCategory: "All Categories",
-    pageSize: 6,
-    currentPage: 1,
-    searchQuery: ""
-  }
+const Products = () => {
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectCategory] = useState("All Categories")
+  const [pageSize, setPageSize] = useState(6)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [searchQuery, setSearchQuery] = useState("")
 
-  componentDidMount() {
+  useEffect(() => {
     const products = getProducts()
     const categories = [{ _id: "", name: "All Categories" }, ...getCategories()]
-    this.setState({ products, categories })
-  }
+    setProducts(products)
+    setCategories(categories)
+    setPageSize(6)
+  }, [])
 
-  handleDelete = product => {
-    const productsBeforeDelete = [...this.state.products]
+  const handleDelete = product => {
+    const productsBeforeDelete = [...products]
     try {
       const products = productsBeforeDelete.filter(p => p !== product)
-      this.setState({ products, searchQuery: "" })
+
+      setProducts(products)
+      setSearchQuery("")
     } catch (err) {
       console.error(err)
-      this.setState({ products: productsBeforeDelete })
+
+      setProducts(productsBeforeDelete)
     }
   }
 
-  handleSelectCat = category => {
-    this.setState({
-      selectedCategory: category,
-      currentPage: 1,
-      searchQuery: ""
-    })
+  const handleSelectCat = category => {
+    setSelectCategory(category)
+    setCurrentPage(1)
+    setSearchQuery("")
   }
 
-  handlePageChanges = page => {
-    this.setState({ currentPage: page })
+  const handlePageChanges = page => {
+    setCurrentPage(page)
   }
 
-  handleSearch = query => {
-    this.setState({
-      searchQuery: query,
-      currentPage: 1,
-      selectedCategory: "All Categories"
-    })
+  const handleSearch = query => {
+    setSearchQuery(query)
+    setCurrentPage(1)
+    setSelectCategory("All Categories")
   }
 
-  render() {
-    const {
-      products: allProducts,
-      categories,
-      selectedCategory,
-      pageSize,
-      currentPage,
-      searchQuery
-    } = this.state
+  let filtered = products
+  searchQuery
+    ? (filtered = products.filter(p =>
+        p.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+      ))
+    : (filtered =
+        selectedCategory && selectedCategory._id
+          ? products.filter(p => p.category._id === selectedCategory._id)
+          : products)
 
-    let filtered = allProducts
-    searchQuery
-      ? (filtered = allProducts.filter(p =>
-          p.name.toLowerCase().startsWith(searchQuery.toLowerCase())
-        ))
-      : (filtered =
-          selectedCategory && selectedCategory._id
-            ? allProducts.filter(p => p.category._id === selectedCategory._id)
-            : allProducts)
+  const productsAfterPagination = paginate(filtered, currentPage, pageSize)
 
-    const products = paginate(filtered, currentPage, pageSize)
+  return (
+    <div className='row my-5'>
+      <div className='col-3 mx-5'>
+        {
+          <ListGroup
+            categories={categories}
+            onSelect={handleSelectCat}
+            selectedCategory={selectedCategory}
+          />
+        }
+      </div>
+      <div className='col'>
+        <h1 className='my-5'>
+          There are {filtered.length}{" "}
+          {selectedCategory.name === "All Categories"
+            ? ""
+            : selectedCategory.name}{" "}
+          products available in the database.
+        </h1>
 
-    return (
-      <div className="row my-5">
-        <div className="col-3 mx-5">
-          {
-            <ListGroup
-              categories={categories}
-              onSelect={this.handleSelectCat}
-              selectedCategory={selectedCategory}
-            />
-          }
-        </div>
-        <div className="col">
-          <h1 className="my-5">
-            There are {filtered.length}{" "}
-            {selectedCategory.name === "All Categories"
-              ? ""
-              : selectedCategory.name}{" "}
-            products available in the database.
-          </h1>
+        <Search onChange={handleSearch} value={searchQuery} />
 
-          <Search onChange={this.handleSearch} value={searchQuery} />
-
-          <div className="row">
-            {products.map(p => (
-              <div key={p._id} className="col-lg-4 col-md-12>">
-                <div className="card mb-4">
-                  <img
-                    className=" card-img-top"
-                    src={p.imageUrl}
-                    alt="Product"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{p.name}</h5>
-                    <p className="card-text">${p.price}</p>
-                    <p className="card-text">
-                      <small className="text-muted">
-                        In stock: {p.numberInStock}
-                      </small>
-                    </p>
-                    <button
-                      className="btn btn-sm btn-danger float-right"
-                      onClick={() => this.handleDelete(p)}
-                    >
-                      Delete
-                    </button>
-                    <Link
-                      className="btn btn-sm mx-2 btn-secondary float-right"
-                      to={`/products/${p._id}`}
-                    >
-                      Edit
-                    </Link>
-                  </div>
+        <div className='row'>
+          {productsAfterPagination.map(p => (
+            <div key={p._id} className='col-lg-4 col-md-12>'>
+              <div className='card mb-4'>
+                <img className=' card-img-top' src={p.imageUrl} alt='Product' />
+                <div className='card-body'>
+                  <h5 className='card-title'>{p.name}</h5>
+                  <p className='card-text'>${p.price}</p>
+                  <p className='card-text'>
+                    <small className='text-muted'>
+                      In stock: {p.numberInStock}
+                    </small>
+                  </p>
+                  <button
+                    className='btn btn-sm btn-danger float-right'
+                    onClick={() => handleDelete(p)}
+                  >
+                    Delete
+                  </button>
+                  <Link
+                    className='btn btn-sm mx-2 btn-secondary float-right'
+                    to={`/products/${p._id}`}
+                  >
+                    Edit
+                  </Link>
                 </div>
               </div>
-            ))}
-          </div>
-          <Pagination
-            itemsCount={filtered.length}
-            onPageChange={this.handlePageChanges}
-            pageSize={pageSize}
-            currentPage={currentPage}
-          />
-          <Link to="/products/new" className="btn btn-secondary btn-block">
-            Add New
-          </Link>
+            </div>
+          ))}
         </div>
+        <Pagination
+          itemsCount={filtered.length}
+          onPageChange={handlePageChanges}
+          pageSize={pageSize}
+          currentPage={currentPage}
+        />
+        <Link to='/products/new' className='btn btn-secondary btn-block'>
+          Add New
+        </Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
+export default Products
