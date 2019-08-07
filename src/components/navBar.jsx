c069bc51e454dc3b1000d72fca7be82dfb4dc96b
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-06 17:32:23 -0700

    Added a fontawesome icon to navbar.#13

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,47 +3,47 @@ import { Link, NavLink } from "react-router-dom"
 
 const NavBar = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
-      <Link className="navbar-brand" to="/">
-        Pig E Store
+    <nav className='navbar navbar-expand-lg navbar-light bg-light mb-5'>
+      <Link className='navbar-brand' to='/'>
+        <i className='fas fa-cash-register' /> E Store
       </Link>
       <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        aria-controls='navbarNav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
       >
-        <span className="navbar-toggler-icon" />
+        <span className='navbar-toggler-icon' />
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/products">
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/products'>
               Products
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/customers">
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/customers'>
               Customers
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/sales">
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/sales'>
               Sales
             </NavLink>
           </li>
         </ul>
-        <ul className="navbar-nav float-right">
-          <li className="nav-item align-right">
-            <NavLink className="nav-link" to="/login">
+        <ul className='navbar-nav float-right'>
+          <li className='nav-item align-right'>
+            <NavLink className='nav-link' to='/login'>
               Login
             </NavLink>
           </li>
-          <li className="navbar-nav align-right">
-            <NavLink className="nav-link" to="/register">
+          <li className='navbar-nav align-right'>
+            <NavLink className='nav-link' to='/register'>
               Register
             </NavLink>
           </li>
